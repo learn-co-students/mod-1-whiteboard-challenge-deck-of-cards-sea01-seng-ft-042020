58944ecb18fd302fc2e40e745f7070ba87bd6385
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-05-06 22:54:57 -0700

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,22 @@
 class Deck
+    attr_accessor :cards
+    def initialize
+        ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+        self.cards = []
+        suits.each{|suit| ranks.each{|rank| self.cards.push(Card.new(suit, rank))}}
+    end
 
+    def choose_card
+        deck_size =self.cards.count
+        self.cards.delete_at(rand(deck_size))
+    end
 end
 
 class Card
-
+    attr_accessor :suit, :rank
+    def initialize(suit, rank)
+        self.suit = suit
+        self.rank = rank
+    end
 end
